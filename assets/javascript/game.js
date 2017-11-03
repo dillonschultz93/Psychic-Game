0b082ca702f72946bc98706109cc38143027986a
Dillon Schultz <dillonschultz93@gmail.com>
2017-11-03 15:44:24 -0700

    Added the logic to the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+// A list of choices for the browser
+
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// A loop that runs through the alphabet using the array above
+//------------------------------------------------------------
+//for (var computerChoiceIndex = 0; computerChoiceIdex < computerChoice.length; computerChoiceIndex++) {
+
+// console.log(computerChoice[computerChoiceIndex]);
+
+//}
+
+// Score keeping
+var wins = 0;
+var losses = 0;
+var attemptsLeft = 5;
+
+//-------------------------------------------------------------------------------------------------
+document.onkeyup = function (event) {
+
+    // User input
+    var userGuess = event.key;
+
+    // The users last guess
+    var guessesSoFar = userGuess;
+
+    // Randomly picks a letter
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+    //Score Keeping
+    if (userGuess === computerGuess) {
+        wins++;
+    } else {
+        attemptsLeft--;
+    }
+
+    if (attemptsLeft === 0) {
+        losses++;
+        attemptsLeft = 5;
+    }
+
+    // Writes to index.html the score keeping variables
+    document.getElementById("guesses-left").innerHTML = attemptsLeft;
+    document.getElementById("guesses-so-far").innerHTML = guessesSoFar;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+};
